850663280c9009d9cf46b920bf6a10dc71db7d20
kapiltaran <kapiltaran@gmail.com>
2018-05-06 15:23:18 +0530

    test java ile

---
+++ JenkinsGitProject/src/Test.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("helo world");
+
+		ArrayList al = new ArrayList();
+		al.add("arrau");
+		System.out.println("array "+al);
+
+	}
+
+}
